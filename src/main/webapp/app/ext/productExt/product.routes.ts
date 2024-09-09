7b7004db0ae52f4fc97a88c8335e5b92badcd089
oueslati00 <boueslati76@gmail.com>
2024-09-09 01:07:42 +0100

    improve code

--- src/main/webapp/app/ext/productExt/product.routes.ts
+++ src/main/webapp/app/ext/productExt/product.routes.ts
@@ -37,7 +37,7 @@ const productRoute: Routes = [
   },
   {
     path: ':id/edit',
-    component: ProductUpdateComponent,
+    component: ProductUpdateComponentExtend,
     resolve: {
       product: ProductResolve,
     },
